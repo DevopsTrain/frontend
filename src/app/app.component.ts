6049c24f0ef514263209e1e51c9876c076c5a823
WLDS <WLDS@R7053.ESG-GMBH.DE>
2017-11-17 16:40:37 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AgmCoreModule} from '@agm/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title: string = 'My first AGM project';
+  lat: number = 48.187498;
+  lng: number = 11.601819;
+  zoom: number = 15;
+  label: string = 'Me';
+  markers: Marker[] = [
+    {
+      lat: 51.673858,
+      lng: 7.815982,
+      label: 'A'
+    },
+    {
+      lat: 51.373858,
+      lng: 7.215982,
+      label: 'B'
+    },
+    {
+      lat: 51.723858,
+      lng: 7.895982,
+      label: 'C'
+    }
+  ];
+  location = {};
+
+  // sets the position of the user
+  setPosition(position) {
+    const location: Location = position.coords;
+    this.lat = location.latitude;
+    this.lng = location.longitude;
+  }
+  ngOnInit() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.setPosition.bind(this));
+      navigator.geolocation.watchPosition(this.setPosition.bind(this));
+    }
+  }
+}
+
+interface Marker {
+  lat: number;
+  lng: number;
+  label?: string;
+}
+
+interface Location {
+  latitude: number;
+  longitude: number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {AgmCoreModule} from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatButtonModule,
+    AgmCoreModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
