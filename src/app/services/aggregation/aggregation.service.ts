7863320912ab266c4adfe3b8fa67f001105ca0b9
swaldinger <33757520+Waldiiii@users.noreply.github.com>
2018-02-06 11:54:28 +0100

    updated aggregation url

--- src/app/services/aggregation/aggregation.service.ts
+++ src/app/services/aggregation/aggregation.service.ts
@@ -10,7 +10,7 @@ import 'rxjs/add/observable/of';
 @Injectable()
 export class AggregationService {
     
-    url: string = 'http://aggregation.eu-central-1.elasticbeanstalk.com/api/vehiclestatus/';
+    url: string = 'http://aggregation.devopstrain.spc5jcoyw7.stackpoint.io/api/vehiclestatus/';
 
   constructor(private http: Http, private coreService: CoreService) { }
   
