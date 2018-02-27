c11af2695b3f7bc68fd044f7e486184425dfb1f7
Tobias Dussmann <tobias.dussmann@partner.bmw.de>
2018-02-27 21:20:29 +0100

    update k8s deployment files for canary

--- deploy/k8s/envsubst.sh
+++ deploy/k8s/envsubst.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # call with build number (image tag for ci built docker image) as single argument
 sed -e "s/\${BUILD_NUMBER}/$1/" deploy-CI.template.yaml  > deploy-CI.yaml
+sed -e "s/\${BUILD_NUMBER}/$1/" deploy-canary-CI.template.yaml  > deploy-canary-CI.yaml
 nl deploy-CI.yaml
+nl deploy-canary-CI.yaml
